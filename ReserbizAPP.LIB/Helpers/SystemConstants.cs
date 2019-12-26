e3b7f9cb464262caf1c64ecc49eaad3cc0ff053d
Roi Larrence Amatong <Roi.Amatong@qlogic.se>
2019-12-26 16:04:46 +0100

    Created Test Project

--- ReserbizAPP.LIB/Helpers/SystemConstants.cs
+++ ReserbizAPP.LIB/Helpers/SystemConstants.cs
@@ -9,7 +9,6 @@ namespace ReserbizAPP.LIB.Helpers
             public const int MONTH_MAX_VALUE = 12;
             public const int QUARTER_MAX_VALUE = 4;
             public const int YEAR_MAX_VALUE = 1;
-
         }
     }
 }
