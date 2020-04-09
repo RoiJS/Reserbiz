12d812a2c2e9fad3c4c78baeb919c40e61d52148
Roi Larrence Amatong <Roi.Amatong@qlogic.se>
2020-04-09 16:51:22 +0200

    apply translate text on side drawer service

--- ReserbizAPP/src/app/_services/side-drawer.service.ts
+++ ReserbizAPP/src/app/_services/side-drawer.service.ts
@@ -1,55 +1,62 @@
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 import { MainMenu } from '../_models/main-menu.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SideDrawerService {
+  private _mainMenu: MainMenu[];
 
-  mainMenu: MainMenu[] = [
-    {
-      text: 'Dashboard',
-      icon: String.fromCharCode(0xf51b),
-      url: '/dashboard',
-      hasSeparator: false
-    },
-    {
-      text: 'Tenants',
-      icon: String.fromCharCode(0xf0c0),
-      url: '/tenants',
-      hasSeparator: false
-    },
-    {
-      text: 'Contracts',
-      icon: String.fromCharCode(0xf573),
-      url: '/contracts',
-      hasSeparator: false
-    },
-    {
-      text: 'Terms',
-      icon: String.fromCharCode(0xf570),
-      url: '/terms',
-      hasSeparator: false
-    },
-    {
-      text: 'Space Types',
-      icon: String.fromCharCode(0xf015),
-      url: '/space-types',
-      hasSeparator: false
-    },
-    {
-      text: 'Profile',
-      icon: String.fromCharCode(0xf2bd),
-      url: '/profile',
-      hasSeparator: false
-    },
-    {
-      text: 'Settings',
-      icon: String.fromCharCode(0xf013),
-      url: '/settings',
-      hasSeparator: true
-    }
-  ];
+  constructor(private translateService: TranslateService) {
+    this._mainMenu = [
+      {
+        text: this.translateService.instant('MAIN_MENU.DASHBOARD'),
+        icon: String.fromCharCode(0xf51b),
+        url: '/dashboard',
+        hasSeparator: false,
+      },
+      {
+        text: this.translateService.instant('MAIN_MENU.TENANTS'),
+        icon: String.fromCharCode(0xf0c0),
+        url: '/tenants',
+        hasSeparator: false,
+      },
+      {
+        text: this.translateService.instant('MAIN_MENU.CONTRACTS'),
+        icon: String.fromCharCode(0xf573),
+        url: '/contracts',
+        hasSeparator: false,
+      },
+      {
+        text: this.translateService.instant('MAIN_MENU.TERMS'),
+        icon: String.fromCharCode(0xf570),
+        url: '/terms',
+        hasSeparator: false,
+      },
+      {
+        text: this.translateService.instant('MAIN_MENU.SPACE_TYPES'),
+        icon: String.fromCharCode(0xf015),
+        url: '/space-types',
+        hasSeparator: false,
+      },
+      {
+        text: this.translateService.instant('MAIN_MENU.PROFILE'),
+        icon: String.fromCharCode(0xf2bd),
+        url: '/profile',
+        hasSeparator: false,
+      },
+      {
+        text: this.translateService.instant('MAIN_MENU.SETTINGS'),
+        icon: String.fromCharCode(0xf013),
+        url: '/settings',
+        hasSeparator: true,
+      },
+    ];
+  }
 
-  constructor() {}
+  public mainMenu(): MainMenu[] {
+    return this._mainMenu;
+  }
 }
