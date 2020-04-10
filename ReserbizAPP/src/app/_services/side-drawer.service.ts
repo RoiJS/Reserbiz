6dd0957a8babf930d01b39f6cd4190bdf5c1f9d1
Roi Larrence Amatong <Roi.Amatong@qlogic.se>
2020-04-10 11:44:47 +0200

    Fix text translation on sidedrawer component

--- ReserbizAPP/src/app/_services/side-drawer.service.ts
+++ ReserbizAPP/src/app/_services/side-drawer.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-
 import { MainMenu } from '../_models/main-menu.model';
 
 @Injectable({
@@ -9,46 +7,46 @@ import { MainMenu } from '../_models/main-menu.model';
 export class SideDrawerService {
    mainMenu: MainMenu[];
 
-  constructor(private translateService: TranslateService) {
+  constructor() {
     this.mainMenu = [
       {
-        text: this.translateService.instant('MAIN_MENU.DASHBOARD'),
+        text: 'MAIN_MENU.DASHBOARD',
         icon: String.fromCharCode(0xf51b),
         url: '/dashboard',
         hasSeparator: false,
       },
       {
-        text: this.translateService.instant('MAIN_MENU.TENANTS'),
+        text: 'MAIN_MENU.TENANTS',
         icon: String.fromCharCode(0xf0c0),
         url: '/tenants',
         hasSeparator: false,
       },
       {
-        text: this.translateService.instant('MAIN_MENU.CONTRACTS'),
+        text: 'MAIN_MENU.CONTRACTS',
         icon: String.fromCharCode(0xf573),
         url: '/contracts',
         hasSeparator: false,
       },
       {
-        text: this.translateService.instant('MAIN_MENU.TERMS'),
+        text: 'MAIN_MENU.TERMS',
         icon: String.fromCharCode(0xf570),
         url: '/terms',
         hasSeparator: false,
       },
       {
-        text: this.translateService.instant('MAIN_MENU.SPACE_TYPES'),
+        text: 'MAIN_MENU.SPACE_TYPES',
         icon: String.fromCharCode(0xf015),
         url: '/space-types',
         hasSeparator: false,
       },
       {
-        text: this.translateService.instant('MAIN_MENU.PROFILE'),
+        text: 'MAIN_MENU.PROFILE',
         icon: String.fromCharCode(0xf2bd),
         url: '/profile',
         hasSeparator: false,
       },
       {
-        text: this.translateService.instant('MAIN_MENU.SETTINGS'),
+        text: 'MAIN_MENU.SETTINGS',
         icon: String.fromCharCode(0xf013),
         url: '/settings',
         hasSeparator: true,
