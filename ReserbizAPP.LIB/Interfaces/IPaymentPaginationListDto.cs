ab63b5f1b39908f7a5b98fccfdf5cbee74eb4288
Roi Larrence Amatong <Roi.Amatong@qlogic.se>
2021-01-01 10:23:26 +0100

    Included missing properties on IPaymentPaginationListDto interface

--- ReserbizAPP.LIB/Interfaces/IPaymentPaginationListDto.cs
+++ ReserbizAPP.LIB/Interfaces/IPaymentPaginationListDto.cs
@@ -3,5 +3,8 @@ namespace ReserbizAPP.LIB.Interfaces
     public interface IPaymentPaginationListDto : IEntityPaginationListDto
     {
         double TotalAmount { get; set; }
+        double TotalAmountFromDeposit { get; set; }
+        double DepositedAmountBalance { get; set; }
+        double SuggestedAmountForPayment { get; set; }
     }
 }
