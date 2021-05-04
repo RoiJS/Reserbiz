5db974cab979ad8918aa766a33def00270b3bc24
Roi Larrence Amatong <Roi.Amatong@qlogic.se>
2021-05-04 20:20:31 +0200

    Activate checking of current environment on web api

--- ReserbizAPP.API/Startup.cs
+++ ReserbizAPP.API/Startup.cs
@@ -187,7 +187,7 @@ namespace ReserbizAPP.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment() == false)
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
