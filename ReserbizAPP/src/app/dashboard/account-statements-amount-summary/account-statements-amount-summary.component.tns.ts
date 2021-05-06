7affc1a856b5b7b411b5d3e6ecfccd4908ad8553
Roi Larrence Amatong <Roi.Amatong@qlogic.se>
2021-05-06 18:25:36 +0200

    #42 - Applied refresh on account statements amount summer widget after registering payments. Wrap function inside ngZone

--- ReserbizAPP/src/app/dashboard/account-statements-amount-summary/account-statements-amount-summary.component.tns.ts
+++ ReserbizAPP/src/app/dashboard/account-statements-amount-summary/account-statements-amount-summary.component.tns.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { ObservableArray } from '@nativescript/core';
 
 import { TranslateService } from '@ngx-translate/core';
+
+import { Subscription } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 import { BaseWidgetComponent } from '@src/app/shared/component/base-widget.component';
 import { AccountStatementsAmountSummary } from '@src/app/_models/account-statement-amount-summary.model';
 import { AccountStatementSummaryChart } from '@src/app/_models/account-statement-summary-chart.model';
 
 import { AccountStatementService } from '@src/app/_services/account-statement.service';
+import { PaymentsService } from '@src/app/_services/payments.service';
 
 @Component({
   selector: 'ns-account-statements-amount-summary',
@@ -19,44 +23,58 @@ import { AccountStatementService } from '@src/app/_services/account-statement.se
 })
 export class AccountStatementsAmountSummaryComponent
   extends BaseWidgetComponent
-  implements OnInit {
+  implements OnInit, OnDestroy {
   private _accountStatementAmountSummary: ObservableArray<AccountStatementSummaryChart>;
+  private _loadListFlagSub: Subscription;
 
   constructor(
     private accountStatementService: AccountStatementService,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private paymentService: PaymentsService,
+    private ngZone: NgZone
   ) {
     super();
   }
 
   ngOnInit() {
-    this._isBusy = true;
-    setTimeout(() => {
-      (async () => {
-        const accountStatementAmountSummary = await this.accountStatementService.getAccountStatementsAmountSummary();
-        const accountStatementAmountSummaryArray: AccountStatementSummaryChart[] = [];
+    this._loadListFlagSub = this.paymentService.loadPaymentListFlag
+      .pipe(delay(2000))
+      .subscribe(() => {
+        (async () => {
+          this.ngZone.run(async () => {
+            this._isBusy = true;
 
-        const totalAmountPaidSummaryChartValue = this.getTotalAmountPaidChartValue(
-          accountStatementAmountSummary
-        );
-        const totalExpectedAmountSummaryChartValue = this.getTotalUnpaidAmountPaidChartValue(
-          accountStatementAmountSummary
-        );
+            const accountStatementAmountSummary = await this.accountStatementService.getAccountStatementsAmountSummary();
+            const accountStatementAmountSummaryArray: AccountStatementSummaryChart[] = [];
 
-        accountStatementAmountSummaryArray.push(
-          totalAmountPaidSummaryChartValue
-        );
-        accountStatementAmountSummaryArray.push(
-          totalExpectedAmountSummaryChartValue
-        );
+            const totalAmountPaidSummaryChartValue = this.getTotalAmountPaidChartValue(
+              accountStatementAmountSummary
+            );
+            const totalExpectedAmountSummaryChartValue = this.getTotalUnpaidAmountPaidChartValue(
+              accountStatementAmountSummary
+            );
 
-        this._accountStatementAmountSummary = new ObservableArray<AccountStatementSummaryChart>(
-          accountStatementAmountSummaryArray
-        );
+            accountStatementAmountSummaryArray.push(
+              totalAmountPaidSummaryChartValue
+            );
+            accountStatementAmountSummaryArray.push(
+              totalExpectedAmountSummaryChartValue
+            );
 
-        this._isBusy = false;
-      })();
-    }, 2000);
+            this._accountStatementAmountSummary = new ObservableArray<AccountStatementSummaryChart>(
+              accountStatementAmountSummaryArray
+            );
+
+            this._isBusy = false;
+          });
+        })();
+      });
+  }
+
+  ngOnDestroy() {
+    if (this._loadListFlagSub) {
+      this._loadListFlagSub.unsubscribe();
+    }
   }
 
   private getTotalAmountPaidChartValue(
