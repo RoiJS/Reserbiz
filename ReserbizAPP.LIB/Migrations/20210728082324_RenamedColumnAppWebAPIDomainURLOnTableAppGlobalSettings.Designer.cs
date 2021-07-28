0dbbaa2a6aae60bb6b580f0ace928cefb27156ca
Roi Larrence Amatong <roi.amatong@qlogic.se>
2021-07-28 19:15:11 +0200

    #91 - Test Automate the sync process for dbReserbizSYSSQL database

--- ReserbizAPP.LIB/Migrations/20210728082324_RenamedColumnAppWebAPIDomainURLOnTableAppGlobalSettings.Designer.cs
+++ ReserbizAPP.LIB/Migrations/20210728082324_RenamedColumnAppWebAPIDomainURLOnTableAppGlobalSettings.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReserbizAPP.LIB.DbContexts;
 
 namespace ReserbizAPP.LIB.Migrations
 {
     [DbContext(typeof(ReserbizDataContext))]
-    partial class ReserbizDataContextModelSnapshot : ModelSnapshot
+    [Migration("20210728082324_RenamedColumnAppWebAPIDomainURLOnTableAppGlobalSettings")]
+    partial class RenamedColumnAppWebAPIDomainURLOnTableAppGlobalSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
