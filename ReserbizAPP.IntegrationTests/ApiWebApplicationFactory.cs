7321320cb708061584990aceed174dddeb74a569
Roi Larrence Amatong <roi.amatong@qlogic.se>
2022-10-09 17:45:07 +0200

    test

--- ReserbizAPP.IntegrationTests/ApiWebApplicationFactory.cs
+++ ReserbizAPP.IntegrationTests/ApiWebApplicationFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +16,7 @@ namespace ReserbizAPP.IntegrationTests
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            Console.WriteLine(Environment.GetEnvironmentVariables());
             builder.ConfigureAppConfiguration(config =>
             {
                 Configuration = new ConfigurationBuilder()
