498f457952a64d485f01527d176939783fa695e3
Roi Larrence Amatong <amatongroi@gmail.com>
2022-10-19 17:28:01 +0200

    add new script

--- ReserbizAPP.IntegrationTests/script.sql
+++ ReserbizAPP.IntegrationTests/script.sql
@@ -38,8 +38,51 @@ CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
+
 ALTER TABLE [dbo].[Clients] ADD  DEFAULT ((2)) FOR [Type]
 GO
+
+-- (3) Create table GeneralInformations
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[GeneralInformations](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SystemUpdateStatus] [bit] NOT NULL,
+	[DateCreated] [datetime2](7) NOT NULL,
+	[DateDeactivated] [datetime2](7) NOT NULL,
+	[DateDeleted] [datetime2](7) NOT NULL,
+	[DateUpdated] [datetime2](7) NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[IsDelete] [bit] NOT NULL,
+ CONSTRAINT [PK_GeneralInformations] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[GeneralInformations] ADD  DEFAULT ('0001-01-01T00:00:00.0000000') FOR [DateCreated]
+GO
+
+ALTER TABLE [dbo].[GeneralInformations] ADD  DEFAULT ('0001-01-01T00:00:00.0000000') FOR [DateDeactivated]
+GO
+
+ALTER TABLE [dbo].[GeneralInformations] ADD  DEFAULT ('0001-01-01T00:00:00.0000000') FOR [DateDeleted]
+GO
+
+ALTER TABLE [dbo].[GeneralInformations] ADD  DEFAULT ('0001-01-01T00:00:00.0000000') FOR [DateUpdated]
+GO
+
+ALTER TABLE [dbo].[GeneralInformations] ADD  DEFAULT (CONVERT([bit],(0))) FOR [IsActive]
+GO
+
+ALTER TABLE [dbo].[GeneralInformations] ADD  DEFAULT (CONVERT([bit],(0))) FOR [IsDelete]
+GO
+
 
 -- (3) Insert test client information
 USE [dbReserbizSYSSQL]
@@ -84,6 +127,27 @@ VALUES
     ,''
 )
 GO
+
+
+-- (4) Insert general information 
+INSERT INTO [dbo].[GeneralInformations]
+           ([SystemUpdateStatus]
+           ,[DateCreated]
+           ,[DateDeactivated]
+           ,[DateDeleted]
+           ,[DateUpdated]
+           ,[IsActive]
+           ,[IsDelete])
+     VALUES
+           (0
+           ,'0001-01-01 00:00:00.0000000'
+           ,'0001-01-01 00:00:00.0000000'
+           ,'0001-01-01 00:00:00.0000000'
+           ,'0001-01-01 00:00:00.0000000'
+           ,1
+           ,0)
+GO
+
 
 -- (4) Create database for integration testing
 CREATE DATABASE ReserbizDeveloperIntegrationTestDB;
